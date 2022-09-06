958c868c861ea6c34fc8d283910cb0ed9080a6a6
Manish Singh Suyal <96225348+Manishsuyal31@users.noreply.github.com>
2022-09-06 21:16:43 +0530

    portfolio

---
+++ src/components/contact/Contact.js
@@ -0,0 +1,72 @@
+import "./Contact.css";
+import Phone from "../../img/phone.png";
+import Email from "../../img/email.png";
+import Address from "../../img/address.png";
+import { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
+
+const Contact = () => {
+  const formRef = useRef();
+  const [done, setDone] = useState(false)
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        "service_vm5fu97",
+        "template_agj9c48",
+        formRef.current,
+        "1a7uVnLDt323B4VPU"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          setDone(true)
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
+  return (
+    <div className="c">
+      <div className="c-bg"></div>
+      <div className="c-wrapper">
+        <div className="c-left">
+          <h1 className="c-title">Let's discuss your project</h1>
+          <div className="c-info">
+            <div className="c-info-item">
+              <img src={Phone} alt="" className="c-icon" />
+              +91 91190 96586
+            </div>
+            <div classNabme="c-info-item">
+              <img className="c-icon" src={Email} alt="" />
+              manishsuyal948@gmail.com
+            </div>
+            <div className="c-info-item">
+              <img className="c-icon" src={Address} alt="" />
+              Nanital, Uttarakhand India
+            </div>
+          </div>
+        </div>
+        <div className="c-right">
+          <p className="c-desc">
+            <b>What’s your story?</b> Get in touch. Always available for
+            freelancing if the right project comes along.
+          </p>
+          <form ref={formRef} onSubmit={handleSubmit}>
+            <input type="text" placeholder="Name" name="from_name" />
+            <input type="text" placeholder="Subject" name="subject" />
+            <input type="text" placeholder="Email" name="from_email" />
+            <textarea rows="5" placeholder="Message" name="message" />
+            <button>Submit</button>
+            {done && "Thank you, your message has been delivered..."}
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
---
+++ src/data.js
@@ -0,0 +1,39 @@
+import slot1 from './img/productImg/slot1.jpg'
+import slot2 from './img/productImg/slot2.jpg'
+import slot3 from './img/productImg/slot3.jpg'
+import slot4 from './img/productImg/slot4.jpg'
+import slot5 from './img/productImg/slot5.jpg'
+import slot6 from './img/productImg/slot6.jpg'
+
+export const products = [
+    {
+      id: 1,
+      img: slot1,
+      link: "http://youtube.com",
+    },
+    {
+      id: 2,
+      img: slot2,
+      link: "http://whatsapp.com",
+    },
+    {
+      id: 3,
+      img: slot3,
+      link: "http://playstore.com",
+    },
+    {
+      id: 4,
+      img: slot4,
+      link: "http://twitter.com",
+    },
+    {
+      id: 5,
+      img: slot5,
+      link: "http://playgames.com",
+    },
+    {
+      id: 6,
+      img: slot6,
+      link: "http://telegram.com",
+    },
+  ];
